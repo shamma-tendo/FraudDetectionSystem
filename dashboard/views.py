afbd1b78ec71ee682b89f1f636e923bb1da04518
wilson <kayzwilson09@gmail.com>
2026-07-20 23:13:05 +0300

    created dashboard layout and statics card

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def dashboard(request):
+
+    context = {
+
+        "total_transactions": 1245,
+
+        "fraud_transactions": 43,
+
+        "safe_transactions": 1202,
+
+        "average_risk": 23,
+
+    }
+
+
+    return render(
+        request,
+        "dashboard/dashboard.html",
+        context
+    )
